fc7c335648ae44d48ac75b8aa549368ac874c2bb
Yumomonteiro <yuriaco@hotmail.com>
2023-03-16 18:11:38 +0000

    Create while.js

---
+++ ex001/repetições/while.js
@@ -0,0 +1,28 @@
+
+console.log('Vai começar...')
+for (var c = 1 ;c <= 5 ; c++) {
+    console.log(c)
+    
+}
+console.log('Fim!')
+/*
+var c = 1
+
+/*do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
