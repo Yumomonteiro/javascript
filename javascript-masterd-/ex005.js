047d3aa35c75573d8ff7fabff4f665abeca60108
Yumomonteiro <yuriaco@hotmail.com>
2023-04-06 09:42:55 +0100

    ex007

--- javascript-masterd-/ex005.js
+++ javascript-masterd-/ex005.js
@@ -1,1 +1,9 @@
-var novo
+var novoArray = ['Carro','mota','barco'];
+console.log('array original' + novoArray);
+novoArray.push('comboio');
+novoArray.unshift('bicicleta');
+console.log('array com mais de dois elementos' + novoArray);
+novoArray.splice(novoArray.indexOf('mota'), 1);
+novoArray.forEach(function(elemento, indice, array) {
+    console.log(elemento, indice)
+})
